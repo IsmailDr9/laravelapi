a119f25db6f389eada1f9e9a758d033a158aebb9
Ismail Darwish <Ismaildarwish92@gmail.com>
2019-05-18 14:02:59 -0700

    add new file

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('register','API\RegisterController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group( function(){
+    Route::resource('books', 'API\BookController') ;
+});
+
+// Route::group(['middleware'=>'auth:api'],function(){
+// 	Route::get('books','API\BookController');
+// });
+
